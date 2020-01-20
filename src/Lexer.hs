84f85fde5b26dc93b2d3626e90f4d20b9694f6a4
Federico Lochbaum <Fede@SCVs-MacBook-Pro.local>
2020-01-20 08:43:36 -0300

    first attempt

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -57,7 +57,38 @@ validWord _    = True
 ------------------------------
 
 tokenizeProgram :: String -> String -> Either Error [Token]
-tokenizeProgram fileName source = tokenizeProgramWithContext (context fileName source) source
+tokenizeProgram fileName source = offsideRules $ tokenizeProgramWithContext (context fileName source) source
+
+offsideRules :: Either Error [Token] -> Either Error [Token]
+offsideRules err@(Left _) = err
+offsideRules (Right ts) = Right (applyOffsideRules ts [])
+
+-- Note1 = < n >
+-- Note2 = { n }
+applyOffsideRules :: [Token] -> [Int] -> [Token]
+-- Case < n > TODO: FIX THE POINTS
+applyOffsideRules ((OffsideToken Note1 n):ts) (m:ms)  | m == n    = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) SemiColon) : (applyOffsideRules  ts (m:ms))
+                                                      | n < m     = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) RightBrace) : (applyOffsideRules ((OffsideToken Note1 n):ts) ms)
+                                                      | otherwise = applyOffsideRules ts (m:ms)
+-- Case { n } TODO: FIX THE POINTS
+applyOffsideRules ((OffsideToken Note2 n) :ts) []                 = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) LeftBrace) : (applyOffsideRules ts [n])
+applyOffsideRules ((OffsideToken Note2 n) :ts) (m:ms) | n > m     = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) LeftBrace) : (applyOffsideRules ts (n:m:ms))
+                                                      | otherwise = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) LeftBrace) : (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) RightBrace) : (applyOffsideRules ((OffsideToken Note1 n):ts) (m:ms))
+-- Case } TODO: FIX THE POINTS
+applyOffsideRules ((Token { position = pos, tokenType = (KToken LeftBrace)}):ts) (0:ms) = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) RightBrace) : (applyOffsideRules ts ms)
+applyOffsideRules ((Token { position = pos, tokenType = (KToken LeftBrace)}):ts) ms     = [] -- Here is parse error
+
+-- Case { TODO: FIX THE POINTS
+applyOffsideRules ((Token { position = pos, tokenType = (KToken LeftBrace)}):ts) ms = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) LeftBrace): applyOffsideRules ts (0:ms)
+
+-- General cases t:ts TODO: FIX THE POINTS
+applyOffsideRules (t:ts) (m:ms)                       | m != 0    = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) RightBrace) : (applyOffsideRules (t:ts) ms)
+                                                      | otherwise = t : (applyOffsideRules ts ms)
+-- Bases cases [] TODO: FIX THE POINTS
+applyOffsideRules [] []                               = []
+applyOffsideRules [] (m:ms)                           | m != 0    = (Token (TPosition (Point 0 0 0) (Point 0 0 0) }) RightBrace) : (applyOffsideRules [] ms)
+                                                      | otherwise = [] -- parse error
+
 
 tokenizeProgramWithContext :: TContext -> String -> Either Error [Token]
 tokenizeProgramWithContext context ""           = Right []
